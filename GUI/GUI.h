8f1024237069425d65ddc3800693fa5d7726b617
Pelegweiss <pelegw200@gmail.com>
2023-10-02 13:03:53 +0300

    Seperated pipe into different project and added back to project with static libary

--- GUI/GUI.h
+++ GUI/GUI.h
@@ -3,8 +3,8 @@
 #include "controls.h"
 #include <thread>
 #include <iostream>
-#include "pipe.h"
 #include "injector.h"
+#include <Pipe/Pipe.h>
 #pragma comment(linker,"\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
