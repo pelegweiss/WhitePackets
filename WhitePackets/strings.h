dd0f7e248bfd96baf3868a35e7efd958d35ef368
Pelegweiss <pelegw200@gmail.com>
2024-02-17 10:53:47 +0200

    Restrcutred strings.h and renamed variables

--- WhitePackets/strings.h
+++ WhitePackets/strings.h
@@ -1,5 +1,8 @@
-unsigned char pLoad [12] = {0x1b, 0x11, 0x25, 0x1c, 0x1c, 0x15, 0x34, 0x19, 0x9, 0x15, 0x22, 0x1b};
-void DecryptXOR(char* encrypted_data, size_t data_length, char* key, size_t key_length);
-char eKey [] = "MxWhitXXey";
-unsigned int pLoadLen = 12;
+char eKey[] = "MxWhitXXey";
 unsigned int eKeyLen = 10;
+//VirtualAlloc encrypted
+unsigned char vAllocEncrypted [12] = {0x1b, 0x11, 0x25, 0x1c, 0x1c, 0x15, 0x34, 0x19, 0x9, 0x15, 0x22, 0x1b};
+unsigned int vAllocEncryptedLen = 12;
+
+void DecryptXOR(char* encrypted_data, size_t data_length, char* key, size_t key_length);
+
