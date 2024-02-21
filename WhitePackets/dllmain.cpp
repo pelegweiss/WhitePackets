f668522250c724c729e0b77d80714799f3bdd7f8
Pelegweiss <pelegw200@gmail.com>
2024-02-21 12:26:50 +0200

    modified dllmain to work with relfactive loading

--- WhitePackets/dllmain.cpp
+++ WhitePackets/dllmain.cpp
@@ -1,7 +1,6 @@
 #include "logic.h"
-DWORD WINAPI HackThread(HMODULE hModule)
-{
-    // Create a console for the DLL.
+
+extern "C" __declspec(dllexport) void RunYourCode(void) {
     AllocConsole();
     FILE* p;
     freopen_s(&p, "CONOUT$", "w", stdout);
@@ -13,23 +12,20 @@ DWORD WINAPI HackThread(HMODULE hModule)
     // Clean up and close the console.
     fclose(p);
     FreeConsole();
-    FreeLibraryAndExitThread(hModule, 0);
-    return 0;
-}
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-    DWORD  ul_reason_for_call,
-    LPVOID lpReserved
-)
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
 }
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) 
+{
+
+		switch (ul_reason_for_call) {
+		case DLL_PROCESS_ATTACH:
+			break;
+		case DLL_THREAD_ATTACH:
+			break;
+		case DLL_THREAD_DETACH:
+			break;
+		case DLL_PROCESS_DETACH:
+			break;
+		}
+		return TRUE;
+	}
--- WhitePackets/logic.h
+++ WhitePackets/logic.h
@@ -3,7 +3,7 @@
 #include <windows.h>
 #include <vector>
 #include <thread>
-#include <Pipe/Pipe.h>
+#include "Pipe/Pipe.h"
 void mainLogic();
 void pipeHandler();
 void messageHandler(pipeMessage message);
